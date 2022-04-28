1f28010f440731039f797fe9dfbc1013d363bd5e
Abolfazl Jamshidi <jmd.abolfazl14@yahoo.com>
2022-04-28 15:37:10 +0430

    Diets categories section with dragging feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,3 +8,42 @@ searchInput.addEventListener("focusin", (e) => {
 searchInput.addEventListener("focusout", (e) => {
   e.target.parentElement.classList.remove("searchActive");
 });
+
+// grabbing effect
+const slider = document.querySelector(".diets__categories");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// start dragging
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  // add class
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+// exiting dragging
+slider.addEventListener("mouseleave", (e) => {
+  isDown = false;
+  // remove class
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", (e) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+// dragging function
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  // when moving
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; // the time to move
+  slider.scrollLeft = scrollLeft - walk;
+});
